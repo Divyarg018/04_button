e98ce9bed41e93125639839d78214390b2b83981
Divya R <kwrdivyaraj100@gmail.com>
2023-11-28 15:12:51 +0530

    Adding test component

---
+++ mui-tutorial/src/Test.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Typography } from '@mui/material';
+
+function Test() {
+    return (
+        <Typography>Hello World</Typography>
+    )
+}
+
+export default Test;
